d0150c5b66b5fb2336a330b1496e3dee7db390ad
hanouf-translate <hanouf-translate>
2026-09-22 10:43:02 +0300

    Create a function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal's triangle of n

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -15,14 +15,3 @@ def pascal_triangle(n):
         res.append(row_res)
     return res
 
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
-
